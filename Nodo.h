8879b00bb76c0473dc6f483c9b1464ddf56d8f83
Aiverson Castaño T <93091122+iverson18@users.noreply.github.com>
2022-10-01 10:52:58 -0500

    Update Nodo.h

--- Nodo.h
+++ Nodo.h
@@ -1,1 +1,23 @@
+#ifndef Nodo_h
+#define Nodo_h
+#include <string>
+using namespace std;
 
+class Nodo{
+    private:
+        string username;
+        string password;
+        Nodo *next;
+
+    public:
+        Nodo();
+        ~Nodo();
+        void setUsername(string user);
+        string getUsername();
+        void setPassword(string pass);
+        string getPassword();
+        void setNext(Nodo *next);
+        Nodo *getNext();
+};
+
+#endif
