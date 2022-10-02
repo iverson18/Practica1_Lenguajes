0afa538efe08a5bb7c1e3a9b78754807793aa095
Aiverson Castaño T <93091122+iverson18@users.noreply.github.com>
2022-10-01 23:53:21 -0500

    Update Nodo.cpp

--- Nodo.cpp
+++ Nodo.cpp
@@ -4,32 +4,45 @@
 
 using namespace std;
 
-Nodo::Nodo(){
-    
-};
+Nodo::Nodo(){ 
+}
+
+Nodo::Nodo(string user, string pass){ 
+    this->next = NULL;
+    this->username = user;
+    this->password = pass;
+}
 
 Nodo::~Nodo(){
-};
+}
 
 void Nodo::setUsername(string user){
     this->username = user;
-};
+}
 
 string Nodo::getUsername(){
     return this->username;
-};
+}
 
 void Nodo::setPassword(string pass){
     this->password = pass;
-};
+}
 
 string Nodo::getPassword(){
     return this->password;
-};
+}
 
 void Nodo::setNext(Nodo *next){
     this->next = next;
-};
+}
+
 Nodo *Nodo::getNext(){
     return this->next;
-};
+}
+
+string Nodo::toString(){
+    string txt = this->username + "\n";
+    txt = txt + "\n"; 
+    txt = txt + this->password + "\n";
+    return txt;
+}
